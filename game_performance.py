582be81ede55163676ce6202be1fa9b3685fc1fa
Mitch Kubina <mtkubina6@gmail.com>
2025-10-28 11:27:09 -0600

    further progress of parsing data

--- game_performance.py
+++ game_performance.py
@@ -1,16 +1,31 @@
 import pandas as pd
+import re
 
 '''
 #https://www.nbastuffer.com/analytics101/game-score/
 Game Score Formula=(Points)+0.4*(Field Goals Made)+0.7*(Offensive Rebounds)+0.3*(Defensive rebounds)
 +(Steals)+0.7*(Assists)+0.7*(Blocked Shots)
 - 0.7*(Field Goal Attempts)-0.4*(Free Throws Missed) - 0.4*(Personal Fouls)-(Turnovers)
-
 do it per minute?
 '''
+
 def game_performance(player, table):
+    def parse_description(phrase):
+        if "STEAL" in phrase:
+            steals += 1
+        elif "REBOUND" in phrase:
+            match = re.search(r'Off:(\d+)\s+Def:(\d+)', phrase)
+            if match:
+                game_off_rebounds = int(match.group(1))
+                game_def_rebounds =  int(match.group(2))
+    
+    steals = 0
     points = 0
-    rebounds = 0
+    off_rebounds = 0
+    def_rebounds = 0
+
+    game_off_rebounds = 0
+    game_def_rebounds = 0
     fouls = 0
     missed_shots = 0
     
@@ -19,25 +34,32 @@ def game_performance(player, table):
     reduced_table = table[table['player'] == player]
     for index, row in reduced_table.iterrows():
         #print(row)
+
+        if row['gameid'] not in gameids:
+            off_rebounds += game_off_rebounds
+            def_rebounds += game_def_rebounds
+
+            game_off_rebounds = 0
+            game_def_rebounds = 0
+            gameids.append(row['gameid'])
         
         match row['type']:
             case 'Made Shot':
                 points += 2
             case 'Rebound':
-                rebounds += 1
+                parse_description(row['desc'])
             case 'Foul':
                 fouls += 1
             case 'Missed Shot':
                 missed_shots += 1
-        
-        if row['gameid'] not in gameids:
-            gameids.append(row['gameid'])
+            case 'NaN':
+                parse_description(row['desc'])
+
 
     print(f"total points: {points}")
     print(f"Total games: {len(gameids)}")
     print(f"Total points per game: {points/len(gameids)}")
-
-    total_score = points + 0.4 * (points/2) + 0.5 * rebounds - 0.7 * missed_shots - 0.4 * fouls
+    total_score = points + 0.4 * (points/2) + 0.7 * off_rebounds + 0.3 * def_rebounds - 0.7 * missed_shots - 0.4 * fouls
     print(f"avg game score per game: {total_score/len(gameids)}")
 
 
